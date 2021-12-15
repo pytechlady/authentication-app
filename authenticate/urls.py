69d5bb3edfef7c8d22a8db054780db94713063aa
newbietechlady <fumex9910@gmail.com>
2021-12-15 19:04:12 +0100

    Added the reset password and forgot password function

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import RegisterView, VerifyEmail, LoginView
+from .views import RegisterView, VerifyEmail, LoginView, LogoutView, ForgotPasswordView, PasswordReset
 
 
 urlpatterns = [
    path('register', RegisterView.as_view(), name = 'register'),
    path('email-verify', VerifyEmail.as_view(), name = 'email-verify'),
-   path('login', LoginView.as_view(), name='login')
+   path('login', LoginView.as_view(), name='login'),
+   path('logout', LogoutView.as_view(), name='logout'),
+   path('forgot-password', ForgotPasswordView.as_view(), name ='forgot-password'),
+   path('reset-password', PasswordReset.as_view(), name='reset-password')
 ]
